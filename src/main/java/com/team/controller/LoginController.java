07c55a6146301e99a28e6b6676e68c20cbf3223a
Minjae Seo <seomj081923@gmail.com>
2026-07-23 15:24:28 +0900

    Update LoginController.java

--- src/main/java/com/team/controller/LoginController.java
+++ src/main/java/com/team/controller/LoginController.java
@@ -3,7 +3,9 @@ package com.team.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.team.service.LoginService;
 
@@ -20,6 +22,12 @@ public class LoginController {
 
     @GetMapping("/login")
     public String loginPage() {
+    	return "Login";
+    }
+    
+    @PostMapping("/login")
+    public String loginPage(@RequestParam String email) {
+    	System.out.println("Email : " + email);
         return "Login";
     }
 }
