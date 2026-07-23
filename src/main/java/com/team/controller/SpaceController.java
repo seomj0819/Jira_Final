23a11ff7fb8e05d63fd2b9b10e194a7135ac804c
KIMSUNGKWAN <khska44@gmail.com>
2026-07-23 12:30:56 +0900

    goto jsp

--- src/main/java/com/team/controller/SpaceController.java
+++ src/main/java/com/team/controller/SpaceController.java
@@ -1,10 +1,13 @@
 package com.team.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.team.dto.SpaceListDto;
 import com.team.service.SpaceService;
 
 @Controller
@@ -20,6 +23,18 @@ public class SpaceController {
 		model.addAttribute("space_title", space_title);
 		
 		return "RowsAndStatus";
+	}
+	
+	@RequestMapping("/spacedetail")
+	public String SpaceDetail(Model model) {
+		
+		SpaceListDto dto = sSvc.showSpaceProfile("ABCD");
+		List<SpaceListDto> list = sSvc.showSpaceList(1);
+		
+		model.addAttribute("dto", dto);
+		model.addAttribute("list", list);
+		
+		return "SpaceDetail";
 	}
 	
 }
