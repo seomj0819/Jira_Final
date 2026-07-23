db388afbfddac9ab094161ffc16a45b3b6f1da56
Minjae Seo <seomj081923@gmail.com>
2026-07-23 17:10:37 +0900

    Update Taskcontroller

--- src/main/java/com/team/controller/TaskController.java
+++ src/main/java/com/team/controller/TaskController.java
@@ -1,7 +1,11 @@
 package com.team.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.team.service.TaskService;
 
@@ -9,4 +13,21 @@ import com.team.service.TaskService;
 public class TaskController {
 	@Autowired
 	TaskService taskService;
+	
+	@GetMapping("/board")
+	public String board(HttpSession session, Model model) {
+		Integer userNo = (Integer) session.getAttribute("userNo");
+		if (userNo == null) {
+		    return "redirect:/login";
+		}
+	
+		String spaceKey = (String) session.getAttribute("spaceKey");
+		if (spaceKey == null) {
+		    spaceKey = "ABCD"; 
+		    session.setAttribute("spaceKey", spaceKey);
+		}
+		
+		model.addAttribute("taskList", taskService.showTaskList(spaceKey));
+		return "Main_board";
+	}
 }
