a9ac0b95651139ba3d79cb7b09d09afb0e390b18
KIMSUNGKWAN <khska44@gmail.com>
2026-07-24 11:19:54 +0900

    aaaa

--- src/main/java/com/team/controller/HomeController.java
+++ src/main/java/com/team/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.team.controller;
 
-import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.team.dto.SpaceListDto;
 import com.team.service.LoginService;
 import com.team.service.SpaceService;
 
